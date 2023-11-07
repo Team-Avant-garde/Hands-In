9f74d9fa085ed3f142237561fcc72d4dba343c95
unknown <panditmckenzie@gmail.com>
2023-11-07 05:02:55 -0800

    Constants

--- lib/Authentication&Registratino/login.dart
+++ lib/Authentication&Registratino/login.dart
@@ -14,9 +14,50 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Lottie.asset(congrats, animate: true, repeat: true),
-      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Flex(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            direction: Axis.vertical,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 40),
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: Colors.black),
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset(appLogo)
+                  )
+                ),
+
+                // Login Inputs
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:30.0, vertical: 10),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 40,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            label: Text('Student Email'),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12)
+                            )
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
--- lib/Constants/constants.dart
+++ lib/Constants/constants.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
 
+// App Logo
+String appLogo = 'assets/AppLogo/hands.png';
 
+
+// Lotttie Files
 String congrats = 'assets/AppLottie/congrats.json';
+
+// Colors
+ const Color white = Colors.white;
+ const Color black = Colors.white;
+
+
+// Border Radius
